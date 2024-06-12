a94f268b50b29cab1b2cd490562acc0208ac8bf6
Yusfulltest <yusfulltest@gmail.com>
2024-06-12 19:50:56 +0100

    nav bar completed

---
+++ JAVA/blogr.js
@@ -0,0 +1,13 @@
+function NavMenus(){
+let Menus = document.getElementById('lineBar');
+if(Menus.style.display =='block'){
+    Menus.style.display='none'
+}
+
+else{
+    Menus.style.display='block'
+}
+
+
+
+}
